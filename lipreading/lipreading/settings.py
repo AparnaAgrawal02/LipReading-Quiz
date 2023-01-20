bf9a65034e04095c2bf843406bdf8e10cf978280
Aparna <sanagrawal10401@gmail.com>
2023-01-20 16:41:19 +0530

    added styling

--- lipreading/lipreading/settings.py
+++ lipreading/lipreading/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zp_gscn$gbny(isu3_&10=egi()o-=80_j^pf)smk!cukcckl2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
         
 # Application definition
